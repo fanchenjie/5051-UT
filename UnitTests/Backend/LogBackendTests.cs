ce8d1ac6502d94079a7a5473a5c818c23b32e771
fanchenjie <fanchenjie@seattleu.edu>
2019-06-10 14:57:24 -0700

    Added LogBackend create valid item test case

--- UnitTests/Backend/LogBackendTests.cs
+++ UnitTests/Backend/LogBackendTests.cs
@@ -1,5 +1,8 @@
 ﻿using HW1c.Backend;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Models;
+using System.Linq;
+using System;
 
 namespace UnitTests.Backend
 {
@@ -7,13 +10,25 @@ namespace UnitTests.Backend
     public class LogBackendTests
     {
         [TestMethod]
-        public void LogBackend_Create_Default_Should_Pass()
+        public void LogBackend_Create_Valid_Item_Should_Pass()
         {
             //Arrage
+            var myTest = LogBackend.Instance;
+            //var myRepository = new LogRepositoryMock();
+
+
+            var oldItem = new LogModel();
+            oldItem.ID = "ID";
+            oldItem.PhoneID = "PhoneID";
+            oldItem.RecordedDateTime = DateTime.Parse("01/23/2019");
+            oldItem.Value = "Value";
 
             //Act
 
+            var newItem = myTest.Create(oldItem);
             //Assert
+
+            Assert.AreEqual(oldItem, myTest.Read(newItem.ID));
         }
     }
 }
