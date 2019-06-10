1bd96b15c967716656a11b3cd5b479e37277b9d1
fanchenjie <fanchenjie@seattleu.edu>
2019-06-10 13:42:53 -0700

    Added ReportViewModel Set LogViewModel Test

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -36,6 +36,23 @@ namespace UnitTests.Models
 
 
         [TestMethod]
+        public void RepoertViewModel_Set_LogViewModel_Default_Should_Pass()
+        {
+
+            //Arrage
+            var myTest = new ReportViewModel();
+            var myLogViewModel = new LogViewModel();
+            myLogViewModel.LogList.Add(new LogModel { PhoneID = "phone"});
+            //Act
+            myTest.LogViewModel = myLogViewModel;
+            var result = myTest.LogViewModel;
+            //Assert
+
+            Assert.AreEqual("phone",result.LogList[0].PhoneID);
+        }
+
+
+        [TestMethod]
         public void RepoertViewModel_Get_NumberOfUsers_Default_Should_Pass()
         {
 
