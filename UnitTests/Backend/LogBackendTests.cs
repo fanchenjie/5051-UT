c94ac25b3e2e7971b56312583482b49fa8e9fe0e
fanchenjie <fanchenjie@seattleu.edu>
2019-06-10 14:00:36 -0700

    Added LogBackendTests.cs to the project

---
+++ UnitTests/Backend/LogBackendTests.cs
@@ -0,0 +1,15 @@
+﻿using HW1c.Backend;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Backend
+{
+    [TestClass]
+    public class LogBackendTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
